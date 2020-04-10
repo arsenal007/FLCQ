e9d2d5811e8dac9c49232fed9ac12f5311d126ab
arsenal007 <fc_arsenal@ukr.net>
2020-04-10 11:22:46 +0200

    uart command to start frequency measurement

--- main.c
+++ main.c
@@ -15,36 +15,6 @@
 #define ti TRISA1 = 1  //sensor is output
 #define to TRISA1 = 0  //sensor is input
 
-void led_yellow_enable( void )
-{
-  RB3 = 1;
-}
-
-void led_yellow_disable( void )
-{
-  RB3 = 0;
-}
-
-void led_green_enable( void )
-{
-  RB4 = 1;
-}
-
-void led_green_disable( void )
-{
-  RB4 = 0;
-}
-
-void led_blue_enable( void )
-{
-  RB5 = 1;
-}
-
-void led_blue_disable( void )
-{
-  RB5 = 0;
-}
-
 void cmnd_w( unsigned char cmnd )
 {  //send command to temperature sensor
   unsigned char i;
@@ -122,66 +92,8 @@ void InitUART( void )
   TXEN = 1;  // Enable the transmitter
 }
 
-uint32_t timer0 = 0;
-
-uint8_t prescaler_timer0 = 0;
-
-void initTimer0( void )
-{
-  //Timer0 Registers Prescaler= 256 - TMR0 Preset = 255 - Freq = 19531.25 Hz - Period = 0.000051 seconds
-  T0CS = 1;  // bit 5  TMR0 Clock Source Select bit...0 = Internal Clock (CLKO) 1 = Transition on T0CKI pin
-  T0SE = 0;  // bit 4 TMR0 Source Edge Select bit 0 = low/high 1 = high/low
-  PSA = 0;   // bit 3  Prescaler Assignment bit...0 = Prescaler is assigned to the Timer0
-
-  if ( prescaler_timer0 & ( 1 << ( 2 ) ) )
-  {
-    PS2 = 1;  // bits 2-0  PS2:PS0: Prescaler Rate Select bits
-  }
-  else
-  {
-    PS2 = 0;
-  }
-
-  if ( prescaler_timer0 & ( 1 << ( 1 ) ) )
-  {
-    PS1 = 1;  // bits 2-0  PS2:PS0: Prescaler Rate Select bits
-  }
-  else
-  {
-    PS1 = 0;
-  }
-
-  if ( prescaler_timer0 & 1 )
-  {
-    PS0 = 1;  // bits 2-0  PS2:PS0: Prescaler Rate Select bits
-  }
-  else
-  {
-    PS0 = 0;
-  }
-
-  timer0 = 0;
-  TMR0 = 0;
-  T0IE = 1;
-  TRISA3 = 1;
-}
-
-void initTimer1( void )
-{
-  // Timer1 Registers:
-  // Prescaler=1:8; TMR1 Preset=59286; Freq=100.00Hz; Period=10,000,000 ns
-  T1CKPS1 = 1;  // bits 5-4  Prescaler Rate Select bits
-  T1CKPS0 = 1;
-  T1OSCEN = 0;  // bit 3 Timer1 Oscillator Enable Control: bit 1=on
-  NOT_T1SYNC =
-      1;  // bit 2 Timer1 External Clock Input Synchronization Control bit: 1=Do not synchronize external clock input
-  TMR1CS =
-      0;  // bit 1 Timer1 Clock Source Select bit: 0=Internal clock (FOSC/4) / 1 = External clock from pin T1CKI (on the rising edge)
-  TMR1IE = 1;
-  TMR1ON = 1;    // bit 0 enables timer
-  TMR1H = 0xB;   // preset for timer1 MSB register
-  TMR1L = 0xDC;  // preset for timer1 LSB register
-}
+uint16_t timer0_overflows;
+uint8_t timer0_prescaler = 5;
 
 void SendByteSerially( unsigned char Byte )  // Writes a character to the serial port
 {
@@ -205,108 +117,53 @@ unsigned char ReceiveByteSerially( void )  // Reads a character from the serial 
 }
 
 #define UART_PACKET_SIZE 16u
-#define RX_FULL 7
 uint32_t end;
 uint8_t uart[ UART_PACKET_SIZE ];
 uint8_t uart_id;
-uint8_t status;
-uint16_t timer1;
-uint8_t lauflicht;
-uint8_t prescaler;  // can be 32, 16, 8, 4, 2, 1
 
-uint8_t decrement_prescaler( void )
-{
-  switch ( prescaler )
+typedef union {
+  uint8_t entire;
+  struct
   {
-    case ( 32u ):
-    {
-      prescaler = 16u;
-      break;
-    }
-    case ( 16u ):
-    {
-      prescaler = 8u;
-      break;
-    }
-    case ( 8u ):
-    {
-      prescaler = 4u;
-
-      break;
-    }
-    case ( 4u ):
-    {
-      prescaler = 2u;
-      break;
-    }
-    case ( 2u ):
-    {
-      prescaler = 1u;
-      break;
-    }
-    default:
-    {
-      prescaler = 32u;
-      break;
-    }
+    unsigned _RX_FULL_ : 1;           // 7
+    unsigned _TIMER0_INTERRUPT_ : 1;  // 6
+    unsigned _LED_GREEN_ : 1;         // 5
+    unsigned _LED_BLUE_ : 1;          // 4
+    unsigned _LED_YELLOW_ : 1;        // 3
+    unsigned _SERIE_ : 1;             // 2
+    unsigned _UNDIFINED1_ : 1;        // 1
+    unsigned _UNDIFINED0_ : 1;        // 1
   };
-  return ( prescaler );
-}
+} FLCQ_STATUS_t;
 
-void SetPrescaler( uint8_t option_reg );
+FLCQ_STATUS_t b;
+
+#define RX_FULL b._RX_FULL_
+#define TIMER1_INTERRUPT b._TIMER0_INTERRUPT_
+#define LED_GREEN b._LED_GREEN_
+#define LED_BLUE b._LED_BLUE_
+#define LED_YELLOW b._LED_YELLOW_
+#define SERIE b._SERIE_
+
+void SetPrescaler( uint8_t _option_reg );
 void PrescalerOff( void );
 
-#define PSC_DIV_BY_2 0b00100000
-#define PSC_DIV_BY_4 0b00100001
-#define PSC_DIV_BY_8 0b00100010
-#define PSC_DIV_BY_16 0b00100011
-#define PSC_DIV_BY_32 0b00100100
+const uint8_t option_reg[ 5 ] = {
+  0b00100000,  // PSC_DIV_BY_2
+  0b00100001,  // PSC_DIV_BY_4
+  0b00100010,  // PSC_DIV_BY_8
+  0b00100011,  // PSC_DIV_BY_16
+  0b00100100   // PSC_DIV_BY_32
+};
 
-void freq_mesure( uint8_t prescaler )
+void freq_mesure_init( void )
 {
-  switch ( prescaler )
-  {
-    case ( 32u ):
-    {
-      SetPrescaler( PSC_DIV_BY_32 );
-      break;
-    }
-    case ( 16u ):
-    {
-      SetPrescaler( PSC_DIV_BY_16 );
-      break;
-    }
-    case ( 8u ):
-    {
-      SetPrescaler( PSC_DIV_BY_8 );
-      break;
-    }
-    case ( 4u ):
-    {
-      SetPrescaler( PSC_DIV_BY_4 );
-      break;
-    }
-    case ( 2u ):
-    {
-      SetPrescaler( PSC_DIV_BY_2 );
-      break;
-    }
-    case ( 1u ):
-    {
-      PrescalerOff();
-      break;
-    }
-  }
-
-  //Timer0 Registers Prescaler= 256 - TMR0 Preset = 255 - Freq = 19531.25 Hz - Period = 0.000051 seconds
-  //T0CS = 1;  // bit 5  TMR0 Clock Source Select bit...0 = Internal Clock (CLKO) 1 = Transition on T0CKI pin
   T0SE = 0;  // bit 4 TMR0 Source Edge Select bit 0 = low/high 1 = high/low
-  timer0 = 0;
-  TMR0 = 0;
-  T0IE = 1;
+  timer0_overflows = 0;
 
   // Timer1 Registers:
-  // Prescaler=1:8; TMR1 Preset=59286; Freq=100.00Hz; Period=10,000,000 ns
+  // Prescaler=1:8; TMR1 Preset=0;
+  // aprox: Freq=9.53674Hz; Period=0.1048576 s
   T1CKPS1 = 1;  // bits 5-4  Prescaler Rate Select bits
   T1CKPS0 = 1;
   T1OSCEN = 0;  // bit 3 Timer1 Oscillator Enable Control: bit 1=on
@@ -315,43 +172,74 @@ void freq_mesure( uint8_t prescaler )
   TMR1CS =
       0;  // bit 1 Timer1 Clock Source Select bit: 0=Internal clock (FOSC/4) / 1 = External clock from pin T1CKI (on the rising edge)
   TMR1IE = 1;
-  TMR1H = 0xB;   // preset for timer1 MSB register
-  TMR1L = 0xDC;  // preset for timer1 LSB register
-                 //
-  TMR1ON = 1;    // bit 0 enables timer
-  TRISA3 = 1;    // TRISA3 = 1 enable it as input
-  led_green_enable();
+  TMR1H = 0;   // preset for timer1 MSB register
+  TMR1L = 0;   // preset for timer1 LSB register
+               //
+  TMR1ON = 1;  // bit 0 enables timer
+
+  TMR0 = 0;
+  T0IE = 1;
+
+  TRISA3 = 1;  // TRISA3 = 1 enable it as input
+}
+
+void freq_mesure( void )
+{
+  if ( timer0_prescaler )
+    SetPrescaler( option_reg[ --timer0_prescaler ] );
+  else
+    PrescalerOff();
+  freq_mesure_init();
+}
+
+void freq_mesure1( void )
+{
+  timer0_prescaler = 5;
+  freq_mesure();
 }
 
 void answer( uint8_t N );
 
-void uart_freq( uint32_t frequency )
+uint8_t serie_counter;
+
+void uart_freq( void )
 {
   uart[ 0 ] = 0x06;
-  uart[ 1 ] = ( frequency >> 24 ) & 0xff;
-  uart[ 2 ] = ( frequency >> 16 ) & 0xff;
-  uart[ 3 ] = ( frequency >> 8 ) & 0xff;
-  uart[ 4 ] = frequency & 0xff;
+  uart[ 1 ] = timer0_prescaler;
+  uart[ 2 ] = TMR0;
+  uart[ 3 ] = ( timer0_overflows >> 8 ) & 0xff;
+  uart[ 4 ] = timer0_overflows & 0xff;
   uart[ 5 ] = 0xFF;
   uart[ 6 ] = 0xFF;
   answer( 7 );
-  led_yellow_enable();
+  if ( serie_counter )
+  {
+    serie_counter--;
+    freq_mesure_init();
+  }
 }
 
+const uint8_t test[ 720 ] = { 0 };
+
+// calls after interrupt of timer1
+// register TMR0, timer0_overflows and timer0_prescaler
+// contains raw data
 void timer1_int( void )
 {
-  TRISA3 = 0;
-  TMR1ON = 0;  // disable timer
-  TMR1IF = 0;
-  T0IE = 0;
-  if ( prescaler == 1u )
-  {
-    uart_freq( ( timer0 * 256 + TMR0 ) * prescaler );
-  }
-  else if ( timer0 < 750 )
-    freq_mesure( decrement_prescaler() );
+  // Max input frequency 4Mhz * 0.1048576 = 419430.4
+  // 419430.4 = 1638 * 256 + 102.4
+  // 1638 maximal overflow count for timer0
+  // we have good resolution,
+  // when we are measuring at input frequencies 2..4Mhz
+  // so when our overflow counter is between 1638/2=819...1638
+
+  if ( timer0_prescaler == 0 )
+    uart_freq();
+  else if ( timer0_overflows < 819 )
+    freq_mesure();  // freq_mesure does always prescaler decrement
   else
-    uart_freq( ( timer0 * 256 + TMR0 ) * prescaler );
+    uart_freq();
+  TIMER1_INTERRUPT = 0;
 }
 
 //void __interrupt() isr( void )
@@ -360,26 +248,31 @@ void isr( void ) __interrupt 0
   // If UART Rx Interrupt
   if ( RCIF )
   {
-    if ( ( ( status >> RX_FULL ) & 1U ) == 0 )
+    if ( RX_FULL )
+    {
+      ReceiveByteSerially();  // dummy read
+    }
+    else
     {
       uart[ uart_id ] = ReceiveByteSerially();
       uart_id++;
       uart_id &= 0b00001111;
-      if ( ( 4 <= uart_id ) && ( uart[ uart_id - 1 ] == 0xFF ) && ( uart[ uart_id - 2 ] == 0xFF ) )
-        status |= ( 1 << RX_FULL );
-    }
-    else
-    {
-      ReceiveByteSerially();
+      if ( ( 4 <= uart_id ) && ( uart[ uart_id - 1 ] == 0xFF ) && ( uart[ uart_id - 2 ] == 0xFF ) ) RX_FULL = 1;
     }
   }
   else if ( TMR0IF )
   {
     TMR0IF = 0;
-    timer0++;
+    timer0_overflows++;
   }
   else if ( TMR1IF )
-    timer1_int();
+  {
+    TRISA3 = 0;
+    TMR1ON = 0;  // disable timer
+    TMR1IF = 0;
+    T0IE = 0;
+    TIMER1_INTERRUPT = 1;
+  }
 }
 
 void SendStringSerially( const unsigned char* st )
@@ -446,23 +339,48 @@ void uart_rx_packet( void )
       send_eeprom_value_to_host();
       break;
     }
+    case ( 0x07 ):
+    {
+      uart[ 1 ]--;
+      serie_counter = uart[ 1 ];
+      freq_mesure1();
+      break;
+    }
   }
-  status &= ~( 1 << RX_FULL );
+
   uint8_t i;
   for ( i = 0; i < UART_PACKET_SIZE; i++ )
     uart[ i ] = 0;
   uart_id = 0;
+  RX_FULL = 0;
+}
+
+void leds( void )
+{
+  if ( LED_YELLOW )
+    RB3 = 1;
+  else
+    RB3 = 0;
+
+  if ( LED_BLUE )
+    RB4 = 1;
+  else
+    RB4 = 0;
+
+  if ( LED_GREEN )
+    RB5 = 1;
+  else
+    RB5 = 0;
 }
 
 void main( void )
 {
-  uint8_t lauflicht = 1;
   TRISB3 = 0;
   TRISB4 = 0;
   TRISB5 = 0;
-  RB3 = 0;
-  RB4 = 0;
-  RB5 = 0;
+  RB3 = 0;  // YELLOW
+  RB4 = 0;  // BLUE
+  RB5 = 0;  // GREEN
 
   InitUART();  // Initialize UART
   //initTimer1();
@@ -479,16 +397,14 @@ void main( void )
   //SendStringSerially( "FLCQ ver. 1.0\n" );  // Send string on UART
 
   // Enable Peripheral Interrupts
-  led_yellow_disable();
-  led_green_disable();
-  led_blue_enable();
-  prescaler = 32u;
   GIE = 1;  // Enable global interrupts
   PEIE = 1;
-  freq_mesure( prescaler );
+
   while ( 1 )
   {
-    if ( ( status >> RX_FULL ) & 1U ) uart_rx_packet();
+    if ( RX_FULL ) uart_rx_packet();
+    if ( TIMER1_INTERRUPT ) timer1_int();
+    leds();
     //__delay_ms( 1000 );
   }
 }
